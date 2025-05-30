581814fb3df1247004e2189a668a8923334499eb
Melvin <melvinmps11301@gmail.com>
2025-05-30 17:51:37 +0300

    slight update

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,10 +6,12 @@ const { InternalServerError } = require("../utils/errors");
 
 exports.getAllCategories = async (req, res, next) => {
   try {
-    const categories = await Category.find().sort({ name: 1 });
+    const categories = await Category.find()
+      .select("name slug parent image description metaTitle metaDescription")
+      .sort({ name: 1 });
+
     sendResponse(res, 200, "Categories fetched successfully", categories);
   } catch (error) {
-    // Optional: Wrap unexpected errors in InternalServerError
     next(new InternalServerError("Failed to fetch categories", error));
   }
 };
